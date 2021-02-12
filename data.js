5999b627b0a22e009a909807429d01389891f8b5
Paul Pritchard <paulp@computerspecialists.co.za>
2021-02-12 10:48:59 +0200

    Added ids to data object

--- data.js
+++ data.js
@@ -1,12 +1,14 @@
 // module.exports = Object.freeze([
 const rawData = [
     {
+        "id": 1,
         "name": "Solar Syatem 1",
         "weight": "XDXDXD tons"
     },
     {
-        "name": "Solar System 22",
-        "weight": "XDXDXD tons"
+        "id": 2,
+        "name": "Solar System 2",
+        "weight": "JUJUJUJ tons"
     }
 ]
 
