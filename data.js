fbd31d478537234f40aee2d6f1bb9795354d352c
Paul Pritchard <pdqsoftware@gmail.com>
2021-02-14 10:09:27 +0200

    Last commit Sunday morning

--- data.js
+++ data.js
@@ -2,13 +2,57 @@
 const rawData = [
     {
         "id": 1,
-        "name": "Solar Syatem 1",
-        "weight": "XDXDXD tons"
+        "name": "Our Solar Syatem",
+        "entities": [
+            {
+                "id": "11",
+                "name": "Mercury",
+                "weight": 12345,
+                "type": "planet"
+            },
+            {
+                "id": "12",
+                "name": "Venus",
+                "weight": 23456,
+                "type": "planet"
+            },
+            {
+                "id": "13",
+                "name": "Earth",
+                "weight": 34567,
+                "type": "planet"
+            },
+            {
+                "id": "14",
+                "name": "Sun",
+                "weight": 999999,
+                "type": "star"
+            }
+        ]
     },
     {
         "id": 2,
-        "name": "Solar System 2",
-        "weight": "JUJUJUJ tons"
+        "name": "Anotehr Solar System",
+        "entities": [
+            {
+                "id": "21",
+                "name": "Planet Zog",
+                "weight": 444,
+                "type": "planet"
+            },
+            {
+                "id": "22",
+                "name": "Garlinga",
+                "weight": 333,
+                "type": "planet"
+            },
+            {
+                "id": "23",
+                "name": "Hatherlingum",
+                "weight": 88888,
+                "type": "star"
+            }
+        ]
     }
 ]
 
