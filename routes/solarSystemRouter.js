2991ea4e1e55aa2295ae7535010a76ac2bfd7edb
Paul Pritchard <pdqsoftware@gmail.com>
2021-02-15 15:57:21 +0200

    Ready to set up hosting on GitHub

--- routes/solarSystemRouter.js
+++ routes/solarSystemRouter.js
@@ -134,6 +134,7 @@ solarSystemRouter.route('/:solarSystemId/weight')
     console.log(`Solar System ID: ${req.params.solarSystemId}`)
     next()
 })
+
 .get((req, res, next) => {
     // Return just the weight of the specified solar system
     const ssId = req.params.solarSystemId
@@ -155,48 +156,99 @@ solarSystemRouter.route('/:solarSystemId/weight')
     res.statusCode = 403
     res.end('POST operation not supported on /solarSystem/' + req.params.solarSystemId + '/weight')
 })
+
 .put((req, res, next) => {
      // Add a status code which will overwrite the one set earlier
      res.statusCode = 403
      res.end('PUT operation not supported on /solarSystem/' + req.params.solarSystemId + '/weight')
 })
+
 .delete((req, res, next) => {
     // Return a simple response
     res.statusCode = 403
-     res.end('DELETE operation not supported on /solarSystem/' + req.params.solarSystemId + '/weight')
+    res.end('DELETE operation not supported on /solarSystem/' + req.params.solarSystemId + '/weight')
 })
 
 
-// ALL THE ABOVE ARE COMPLETE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 
+
+// #################################################
 solarSystemRouter.route('/:solarSystemId/planet')
+// #################################################
 
 // This handles ALL calls, no matter what the verb, for the /solarSystem/:solarSystemId/planet type API
 .all((req, res, next) => {
     console.log(`Solar System ID: ${req.params.solarSystemId}`)
     next()
 })
+
 .get((req, res, next) => {
-    res.end('Will send details of all the planets in solar system \'' + req.params.solarSystemId + '\' to you.')
+    // Return a list of planets for specified solar system
+    const ssId = req.params.solarSystemId
+    const newData = utils.getPlanets(ssId, '', rawData)
+    if (newData[0]) {
+        res.setHeader('Content-Type', 'application/json')
+        res.json(newData[1])
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'application/json')
+        res.json({"Error": newData[1]})
+    }
+    // res.end('Will send details of all the planets in solar system \'' + req.params.solarSystemId + '\' to you.')
 })
+
 .post((req, res, next) => {
     // Will contain a body in json format
-    // The body must include a name and description properties
-    res.write('Will add the planet ' + req.params.planetId + '\n')
-    res.write('To the solar system: ' + req.parames.solarSystemId + '\n')
-    res.end('With Name: ' + req.body.name + ' and Weight: ' + req.body.weight)
+    // The body must include id, name,weight and type properties
+    const newPlanet = {
+        "id": req.body.id,
+        "name": req.body.name,
+        "weight": req.body.weight,
+        "type": "planet"
+    }
+    const newData = utils.addPlanet(req.params.solarSystemId, newPlanet, rawData)
+    if (newData[0]) {
+        res.setHeader('Content-Type', 'application/json')
+        res.json(newData[1])
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'application/json')
+        res.json({"Error": newData[1]})
+    }
+    // res.write('Will add the planet ' + req.body.id + '\n')
+    // res.write('To the solar system: ' + req.params.solarSystemId + '\n')
+    // res.end('With Name: ' + req.body.name + ' and Weight: ' + req.body.weight)
 })
+
 .put((req, res, next) => {
     // Add a status code which will overwrite the one set earlier
     res.statusCode = 403
     res.end('PUT operation not supported on /solarSystem/' + req.params.solarSystemId + '/planet')
 })
+
 .delete((req, res, next) => {
-    // Return a simple response
-    res.end('Deleting all planets in solar system: ' + req.params.solarSystemId)
+    // Delete ALL planets in specified solar system
+
+    const ssId = req.params.solarSystemId
+    const newData = utils.deletePlanet(ssId, '', rawData)
+
+    if (newData[0]) {
+        // If success then update raw data and nothing else
+        rawData = newData[1]
+        res.end()
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'application/json')
+        res.json({"Error": newData[1]})
+    }
+    // res.end('Deleting all planets in solar system: ' + req.params.solarSystemId)
 })
+
+
+// ALL THE ABOVE ARE COMPLETE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
 
 solarSystemRouter.route('/:solarSystemId/planet/:planetId')
@@ -207,20 +259,36 @@ solarSystemRouter.route('/:solarSystemId/planet/:planetId')
     console.log(`Planet ID: ${req.params.planetId}`)
     next()
 })
+
 .get((req, res, next) => {
-    res.end('Will send details of planets + \'' + req.params.planetId + '\' in solar system \'' + req.params.solarSystemId + '\' to you.')
+    const ssId = req.params.solarSystemId
+    const newData = utils.getPlanets(ssId, req.params.planetId, rawData)
+    if (newData[0]) {
+        res.setHeader('Content-Type', 'application/json')
+        res.json(newData[1])
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'application/json')
+        res.json({"Error": newData[1]})
+    }
+
+
+    res.end('Will send details of planet + \'' + req.params.planetId + '\' in solar system \'' + req.params.solarSystemId + '\' to you.')
 })
+
 .post((req, res, next) => {
     // Add a status code which will overwrite the one set earlier
     res.statusCode = 403
     res.end('POST operation not supported on /solarSystem/' + req.params.solarSystemId + '/planet/' + req.params.planetId)
 })
+
 .put((req, res, next) => {
     // Use res.write() as many times as required to write long messages
     res.write('Updating the planet: ' + req.params.planetId + '\n')
     res.write('Within the solar system: ' + req.params.solarSystemId + '\n')
     res.end('With name: ' + req.body.name + ' and weight: ' + req.body.weight)
 })
+
 .delete((req, res, next) => {
     // Return a simple response
     res.end('Deleting planet: ' + req.params.planetId + ' in solar system: ' + req.params.solarSystemId)
--- utils/utils.js
+++ utils/utils.js
@@ -62,6 +62,43 @@ module.exports = {
         }
         return {"Error": "Entered solar system Id already exists"}
     },
+    addPlanet(solarSystemId, planetData, rawData) {
+        // Add a new planet to specified solar system
+        console.log(solarSystemId)
+        console.log(planetData)
+        // Check the solar system exists
+        const foundIt = !!rawData.find(x => x.id == solarSystemId)
+        console.log(`FountIt: ${foundIt}`)
+        if (foundIt) {
+            // Solar system match - check planet doesn't already exist
+            console.log('Inside foundIt')
+
+            let errorText = ''
+            rawData.forEach((ss) => {
+                if (ss.id == solarSystemId) {
+                    // Match on solar system
+                    // Does planet already exist?
+                    const foundPlanet = !!ss.entities.find(x => x.id == planetData.id)
+                    console.log(`foundPlanet: ${foundPlanet}`)
+
+                    if (foundPlanet) {
+                        errorText = "Planet with Id '" + planetData.id + "' already exists in solar system '" + solarSystemId + "'"
+                    } else {
+                        // Planet doesn't exist in this solar system - so add it
+                        ss.entities.push(planetData)
+                    }
+                }
+            })
+            // End of loop
+            if (errorText === '') {
+                return ([true, planetData])
+            } else {
+                return ([false, errorText])
+            }
+        }
+        return ([false, 'Solar system \'' + solarSystemId + '\' does not exist'])
+
+    },
     deleteSolarSystem(solarSystemId, rawData) {
         // Delete specified solar system
         // Make sure it really exists
@@ -79,6 +116,37 @@ module.exports = {
             return ([true, newRawData, ])
         }
         return ([false, undefined])
+    },
+    deletePlanet(solarSystemId, planetId, rawData) {
+        // Delete planet(s) from solar system
+        let newData = []
+         // Make sure solar system exists
+         const foundIt = !!rawData.find(x => x.id == solarSystemId)
+         if (foundIt) {
+             // Loop through solar systems, storing data
+             rawData.forEach((ss) => {
+                if (ss.id == solarSystemId) {
+                    // Match
+                    if (planetId) {
+                        // Delete specified planet from this solar system
+
+                    } else {
+                        // Delete all planets in this solar system
+                        let copySolarSystem = Object.assign({}, ss)
+                        delete copySolarSystem.entities
+                        newData.push(copySolarSystem)
+                    }
+                } else {
+                    // Just store data
+                    newData.push(ss)
+                }
+             })
+             return ([true, newData])
+
+         } else {
+             return ([false, "Solar System '" + solarSystemId + "' does not exist"])
+         }
+
     },
     updateSolarSystem(solarSystemId, solarSystemName, rawData) {
         // Updates name of solar system
@@ -118,13 +186,43 @@ module.exports = {
                     // Match
                     updatedObject = Object.assign({}, ss)
                     console.log(`Calculating: ${updatedObject.id}`)
-                    ss.totalWeight = calculateTotalWeight(updatedObject.entities)
+                    updatedObject.totalWeight = calculateTotalWeight(updatedObject.entities)
                     delete updatedObject.entities
                 }
             })
             return([true, updatedObject])
         } else {
             return ([false, undefined])
+        }
+
+    },
+    getPlanets(solarSystemId, planetId, rawData) {
+        // Return a list of planet(s), depending on parameter 2
+        // Make sure the solar system exists
+        const foundIt = !!rawData.find(x => x.id == solarSystemId)
+        let errorText = ''
+        if (foundIt) {
+            let returnObject = []
+            rawData.forEach((ss) => {
+                if (ss.id == solarSystemId) {
+                    // Match - store planet info - if any
+                    if (ss.entities) {
+                        ss.entities.forEach((entity) => {
+                            if (entity.type === "planet" && (planetId === '' || planetId == entity.id)) {
+                                returnObject.push(entity)
+                            }
+                        })
+                    } else {
+                        errorText = "There are no planets in solar system '" + solarSystemId + "'"
+                    }
+                    if (returnObject.length === 0 && !errorText) {
+                        errorText = "Planet '" + planetId + "' does not exist in solar system '" + solarSystemId + "'"
+                    }
+                }
+            })
+            return ([!errorText, errorText || returnObject])
+        } else {
+            return ([false, "Solar system '" + solarSystemId + "' does not exist"])
         }
 
     }
@@ -136,12 +234,17 @@ module.exports = {
 
 function calculateTotalWeight(entities) {
     // Total all entities in this solar system
-
-    console.log(entities.length)
+    // First check there ar some
+    console.log(`entities: ${entities}`)
     let totalWeight = 0
-    entities.forEach((entity) => {
-        totalWeight += entity.weight
-    })
+    if (entities) {
+
+        console.log(entities.length)
+    
+        entities.forEach((entity) => {
+            totalWeight += entity.weight
+        })
+    }
 
     return totalWeight
     
